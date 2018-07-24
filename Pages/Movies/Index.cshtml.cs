323ab0ae4b38151a2f784a2740e5bd6619db8192
Rahul Kumar <rahul@thoughtnirvana.com>
2018-07-25 00:13:18 +0530

    Added search

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 
 namespace RazorPagesMovie.Pages.Movies
 {
@@ -19,10 +21,28 @@ namespace RazorPagesMovie.Pages.Movies
         }
 
         public IList<Movie> Movie { get;set; }
+        public SelectList Genres { get; set; }
+        public string MovieGenre { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string searchString, string movieGenre)
         {
-            Movie = await _context.Movie.ToListAsync();
+            IQueryable<string> genreQuery = from movie in _context.Movie
+                                            orderby movie.Genre
+                                            select movie.Genre;
+            var movies = from movie in _context.Movie
+                         select movie;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
+            if (!String.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(movie => movie.Genre == movieGenre);
+            }
+
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
         }
     }
 }
